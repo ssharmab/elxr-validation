9e824809e3251d6cd2761f16a9e38daf1a3a2c36
Joey Lorich <git@jlx.me>
2024-09-03 08:45:39 -0400

    Simplify approach, tests are running!

--- src/main.py
+++ src/main.py
@@ -1,20 +1,70 @@
 import os
 import sys
 import logging
-
-logger = logging.getLogger()
-
-logger.debug("Start Validation Program")
+import uuid
 
 # Add lava to python path
 sys.path.append(os.path.join(os.path.dirname(__file__), "../lava"))
 
-from windriver.cli.elxr import Validator
+from lava_common.yaml import yaml_safe_load
+from lava_common.log import YAMLLogger
+from lava_dispatcher.device import NewDevice
+from lava_dispatcher.parser import JobParser
+from lava_dispatcher.job import Job
 
-def main():
-    validator = Validator()
-    validator.validate('job.yaml')
+logging.setLoggerClass(YAMLLogger)
+logging.basicConfig(level=logging.DEBUG)
 
-main()
+class ResultsFilter(logging.Filter):
+    """
+    A log filter which only returns lava results
+    """
+    def filter(self, record):
+        msg = yaml_safe_load(record.msg)
+        return msg['lvl'] == "results"
 
-logger.debug("End Validation Program")
+class ResultsHandler(logging.Handler):
+    """
+    A results log handler which takes lava test results and appends them to a results array
+    """
+    def __init__(self, level=logging.INFO, results = []):
+        super().__init__(level)
+        self._results = results
+        self.addFilter(ResultsFilter(__file__))
+
+    def emit(self, record):
+        record_msg = yaml_safe_load(record.msg)
+        result = record_msg['msg']
+        self._results.append(result)
+
+class Validator:
+    """
+    The eLxr Validator
+    """
+    def __init__(self):
+        self._results = []
+        
+        results_handler = ResultsHandler(results = self._results)
+        self._logger = logging.getLogger("dispatcher")
+        self._logger.addHandler(results_handler)
+        
+    def validate(self, job_path, device_path):
+        job_definition = open(job_path, 'r').read()
+        device = NewDevice(device_path)
+        parser = JobParser()
+
+        job : Job =  parser.parse(
+            job_definition,
+            device,
+            str(uuid.uuid4())[:8],
+            logger=self._logger,
+            dispatcher_config=None,
+            env_dut=None,
+        )
+        
+        job.validate()
+        job.run()
+        print(self._results)
+
+validator = Validator()
+validator.validate('job.yaml', "device.yaml")
